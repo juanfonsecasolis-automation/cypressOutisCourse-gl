2783b0d572b89be523380996ec00c519ad91abd8
Juan Fonseca <juanma2268@gmail.com>
2021-04-03 12:09:23 -0600

    Added a new TC

--- cypress/integration/examples/a_simple.spec.js
+++ cypress/integration/examples/a_simple.spec.js
@@ -1,16 +1,12 @@
 // 2021 Juan Fonseca
 
-describe('My first simple test with cypress', () => {
-    it('True should be true', () => {
+describe('Verify system handles invalid credentials	', () => {
+    it('Visit the sauce demo webpage', () => {
         cy.visit('https://www.saucedemo.com/',{timeout: 10000})
     })
 
-    it('should wait for 1 second',() => {
+    it('Should wait for 1 second',() => {
         cy.waitFor(1000)
-    })
-
-    it('Should check correct url',() => {
-        cy.url().should('include','sauce')
     })
 
     it('Should enter invalid credentials',() => {
@@ -22,6 +18,27 @@ describe('My first simple test with cypress', () => {
     it('Should display only one error message', () => {
         cy.get('h3')
             .contains('Epic sadface: Username and password do not match any user in this service')
-            .its('length').should('eq',1)
     })
+})
+
+describe('Verify system handles valid credentials',() => {
+    it('Visit the sauce demo webpage', () => {
+        cy.visit('https://www.saucedemo.com/',{timeout: 10000})
+
+    })
+    
+    it('Should enter valid credentials',() => {
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')   
+        cy.get('#login-button').click()
+    })
+
+    it('Should check correct url',() => {
+        cy.url().should('include','/inventory.html')
+    })
+
+    it('Should count a maximum of 6 items displayed',() => {
+        cy.get('.inventory_item').its('length').should('eq',6)
+    })
+
 })
