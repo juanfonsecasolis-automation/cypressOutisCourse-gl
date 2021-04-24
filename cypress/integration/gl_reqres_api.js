4d22e7e605f94422b2c9a8af7baa17c4e1ca796a
Juan Fonseca <juanma2268@gmail.com>
2021-04-23 20:27:16 -0600

    Finished TCs 10 and 8

--- cypress/integration/gl_reqres_api.js
+++ cypress/integration/gl_reqres_api.js
@@ -1,11 +1,11 @@
-describe('Test 8: Verify pagination works by returning 6 user at the time', () => {
+describe('Test 8: Verify pagination returns the same number of items specified on the "per_page" property	', () => {
 
-    it('should invoke the GET request and verify 6 users were returned', () => {
+    it('should invoke the GET request and verify per_page users were returned', () => {
         // TODO: find a way to add a second baseUrl (like $ npm run cypress:open --config "baseUrl=myUrl")
         cy.request('https://reqres.in/api/users?page=1').then((response) => {   
             expect(response.status).to.eq(200)
-            expect(response.body).to.have.property('per_page')
-            expect(response.body).property('data').property('length').eq(6)
+            expect(response.body).property('data').property('length')
+                .eq(response.body.per_page)
         })
     })
 
@@ -17,12 +17,24 @@ describe('Test 9: verify that each user returned contains personal information',
         cy.request('https://reqres.in/api/users?page=1').its('body').as('body')
         cy.get('@body').its('data').then(int => {
             int.forEach(element => {
-                cy.log(element.email)
-                expect(element).to.have.property('id')
                 expect(element).to.have.property('email')
-                expect(element).to.have.property('first_name')
-                expect(element).to.have.property('last_name')
-                expect(element).to.have.property('avatar')
+            });
+        })
+        
+    })
+
+})
+
+describe('Test 10: verify that each user returned contains personal information', () => {
+
+    it('should invoke the GET request and check the fields', () => {
+        cy.request('https://reqres.in/api/users?page=1').its('body').as('body')
+        cy.get('@body').its('data').then(int => {
+            int.forEach(element => {
+                var pattern = /[a-zA-Z0-9]@[a-zA-Z0-9].[a-z]+/g;
+                var match = element.email.match(pattern)
+                cy.log(match) 
+                expect(match).is.not.null
             });
         })
         
